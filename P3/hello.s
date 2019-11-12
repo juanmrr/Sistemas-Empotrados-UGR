d0271cd44d0face640fe6db38e2b76778ed9b891
Juan Manuel Rubio Rodriguez <juanmrr89@gmail.com>
2019-11-12 12:35:59 +0100

    p3 corregida

--- P3/hello.s
+++ P3/hello.s
@@ -4,24 +4,24 @@
 @
 
 @
-@ Constantes
+@ Variables globales
 @
 
 .data
 
         @ El led rojo está en el GPIO 44 (el bit 12 de los registros GPIO_X_1)
-        LED_RED_MASK: .word     (1 << (44-32))
+        LED_RED_MASK:   .word     (1 << (44-32))
 
         @ El led verde está en el GPIO 45 (el bit 13 de los registros GPIO_X_1)
-        LED_GREEN_MASK: .word     (1 << (45-32))
+        LED_GREEN_MASK: .word   (1 << (45-32))
 
         @ Retardo para el parpadeo
-        DELAY: .word            0x00200000
+        DELAY:          .word   0x00200000
 
-        PIN_BUTTON_1: .word 0x08000000
-        PIN_BUTTON_2: .word 0x04000000
+        PIN_BUTTON_1:   .word   0x08000000
+        PIN_BUTTON_2:   .word   0x04000000
 
-        PIN_BUTTONS: .word 0x00C00000
+        PIN_BUTTONS:    .word   0x00C00000
 
 @
 @ Punto de entrada
@@ -42,20 +42,20 @@ loop:
         @ Pausa corta
         ldr     r0, =DELAY
 	ldr	r0, [r0]
-        bl      pause
+        bl pause
 
 	str	r5, [r6]
 
         @ Pausa corta
         ldr     r0, =DELAY
 	ldr	r0, [r0]
-        bl      pause
+        bl pause
 
         @ Apagamos el led
 	str	r5, [r7]
 
         @ Bucle infinito
-        b       loop
+        b loop
         
 @
 @ Función que produce un retardo
@@ -64,26 +64,26 @@ loop:
         .type   pause, %function
 pause:
         subs    r0, r0, #1
-        bne     pause
+        bne pause
         mov     pc, lr
 
         .type test_buttons, %function
 test_buttons:
         
-        ldr r1, =gpio_data0
-	ldr r1, [r1]        
+        ldr     r1, =gpio_data0
+	ldr     r1, [r1]        
 
-	ldr r9, =PIN_BUTTON_1
-	ldr r9, [r9]
-        tst r1, r9
+	ldr     r9, =PIN_BUTTON_1
+	ldr     r9, [r9]
+        tst     r1, r9
         bne led_green_on
 
-	ldr r9, =PIN_BUTTON_2
-	ldr r9, [r9]
-        tst r1, r9
+	ldr     r9, =PIN_BUTTON_2
+	ldr     r9, [r9]
+        tst     r1, r9
         bne led_red_on
 
-	mov pc, lr
+	mov     pc, lr
 
         .type led_red, %function
 led_red_on:
@@ -110,7 +110,7 @@ gpio_init:
 
         ldr     r8, =gpio_dir_set1
 	//ldr	r8, =GPIO_PAD_DIR1
-	ldr	r8, [r8]
+	//ldr	r8, [r8]
 
         @ Configuramos el GPIO45 para que sea de salida
         ldr     r4, =LED_GREEN_MASK
@@ -124,12 +124,12 @@ gpio_init:
 
         @ Direcciones de los registros GPIO_DATA_SET1 y GPIO_DATA_RESET1
         ldr     r6, =gpio_data_set1
-	ldr	r6, [r6]
+	//ldr	r6, [r6]
         ldr     r7, =gpio_data_reset1
-	ldr	r7, [r7]      
+	//ldr	r7, [r7]      
         
         ldr     r2, =gpio_data_set0
-	ldr	r2, [r2]
+	//ldr	r2, [r2]
         ldr     r3, =PIN_BUTTONS
 	ldr	r3, [r3]
         str     r3, [r2]
